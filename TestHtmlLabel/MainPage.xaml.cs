1143dc82e647e11f448bc52404c11830dfc3db89
Andrei Dinca <>
2023-09-27 11:52:29 +0300

    First commit

---
+++ TestHtmlLabel/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestHtmlLabel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            HtmlLabel.Text = "This is <strong style=\"color:red\">HTML</strong> text.";
+            HtmlLabel2.Text = "Please verify your Operator registration number <a href=\"https://www.google.com/\" target=\"_blank\">here</a>";
+        }
+    }
+}
+
